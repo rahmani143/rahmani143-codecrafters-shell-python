d6d0dc4923a0ff3efc15a47852e11e6bc3ce66db
rahmani143 <bss10i19ibrahimazeem@gmail.com>
2026-05-26 02:30:32 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,38 @@
 import sys
+from pathlib import Path
+import os
 
+def find_in_path(command_path):
+    path_env = os.environ.get("PATH", "")
+    if not path_env:
+        return None
+
+    directories = path_env.split(os.pathsep)
+    for directory in directories:
+        if not os.path.isdir(directory):
+            continue
+        fullpath = os.path.join(directory,command_path)
+
+        if os.path.isfile(full_path) and os.access(fullpath, os.X_OK):
+            return fullpath
+    return None
+
+def handle_type(cmd_arg, builtin_cmds):
+    if cmd_arg in builtin_cmds:
+        print(f"{cmd_arg} is a shell builtin")
+        return
+    
+    path_location = find_in_path(cmd_arg)
+    if path_location:
+        print(f"{cmd_arg} is {path_location}")
+        return
+    
+    print(f"{cmd_arg}: not found")
 
 def main():
     builtin_cmd = {
-        "exit": lambda user_input:sys.exit(0),
-        "type": lambda cmd: print(f"{cmd} is a shell builtin")
-        if cmd in builtin_cmd
-        else print(f"{cmd}: not found"),
+        "exit": lambda args:sys.exit(0),
+        "type": lambda args:handle_type(args) ,
         "echo": lambda args:print(args),
     }
     while True:
