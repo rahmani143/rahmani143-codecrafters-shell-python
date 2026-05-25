be035e8177c61c0ad92625c6231f134741f373ce
rahmani143 <bss10i19ibrahimazeem@gmail.com>
2026-05-26 01:37:06 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,33 +2,21 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    while(True):
-        sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
-            break
-        commands = ["echo","exit","type"]
-        part = command.split(maxsplit = 2)
-        first = part[0]
-        second = part[1]
-        if first == "type":
-            
-            if second in commands:
-                print(f"{second} is a shell builtin")
-            else:
-                print(f"{part[1]}: not found")
-        if first == "echo":
-            argument = " ".join(part[1:])
-            print(argument)
-            # if first == "type":
-            #     if part[1] in commands:
-            #         print(f"{part[1]} is a shell builtin")
-            #     else:
-            #         print(f"{part[1]}: not found")
-            # else:
-            #     print(f"{command}: command not found")
-    pass
+    builtin_cmd = {
+        "exit": lambda user_input:sys.exit(0),
+        "type": lambda cmd: print(f"{cmd} is a shell builtin")
+        if cmd in builtin_cmd
+        else print(f"{cmd}: not found"),
+        "echo": lambda args:print(args),
+    }
+    while True:
+        print("$ ",end="")
+        command, *args = input().strip().split()
+
+        if command in builtin_cmd:
+            builtin_cmd[command](" ".join(args))
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
