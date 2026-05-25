d37d56f2a56a68fa9a7efee233cb5264fb718f9d
rahmani143 <bss10i19ibrahimazeem@gmail.com>
2026-05-26 00:59:46 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,12 +6,18 @@ def main():
     while(True):
         sys.stdout.write("$ ")
         command = input()
+        commands = ["echo","exit"]
         part = command.split(maxsplit = 1)
         first = part[0]
         if command == "exit":
             break
         if first == "echo":
             print(part[1])
+        if first == "type":
+            if part[1] in commands:
+                print(f"{part[1]} is a shell builtin")
+            else:
+                print(f"{part[1]}: not found")
         else:
             print(f"{command}: command not found")
     pass
